130770b39e477beeda3c6f9511b13878e15d22f4
akikohatayama <akikohatayama@gmail.com>
2016-02-19 21:43:29 +0900

    2016/02/19

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //入力欄
+    @IBOutlet weak var inputName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //キーボードをしまう
+        self.inputName.delegate = self
+        //完了ボタン表示
+        inputName.returnKeyType = UIReturnKeyType.Done;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destinationViewController as! ResultViewController
+        
+        resultViewController.outputName = inputName.text!
+        
+    }
+    
+    //キーボードをしまう
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+        inputName.text = ""
+    }
 
 }
 
